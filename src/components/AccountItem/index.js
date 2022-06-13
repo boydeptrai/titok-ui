9f311a4dbafbfa792c14b09e73a08ae18e4aa99a
Tuan Tran <langthamht047@gmail.com>
2022-06-12 21:11:11 -0700

    handle search interface

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,26 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/3acf5d722996ccc1ce3ce070d6d1d442~c5_100x100.jpeg?x-expires=1655082000&x-signature=PB50uHYjIrsgZrNa7HaV3pYOM%2Fw%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className ={cx('check')} icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
